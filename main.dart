365ce398cfdba3c59e2dd06a4118f167e458ae1f
mfarhansheikh <mfaarhansheikh@gmail.com>
2025-10-22 19:29:17 +0500

    task 01 done

---
+++ main.dart
@@ -0,0 +1,11 @@
+void main(){
+    int marks = 40;
+    double attendance = 75.0;
+    if( marks >= 40 && attendance >=75.0){
+        print ("pass");
+
+    }
+    else{
+        print ("fail");
+    }
+}
