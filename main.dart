326ef8d64a33b5b1d2645328f729ba2a9b576fc7
mfarhansheikh <mfaarhansheikh@gmail.com>
2025-10-24 19:11:18 +0500

    assignment compelet

--- main.dart
+++ main.dart
@@ -1,11 +1,140 @@
 void main(){
-    int marks = 40;
-    double attendance = 75.0;
-    if( marks >= 40 && attendance >=75.0){
-        print ("pass");
+   //           TASK 01          //
+
+    // int marks = 55;
+    // double attendance = 75.0;
+    // if( marks >= 40 && attendance >=75.0){
+    //     print ("pass");
+
+    // }
+    
+    // else{
+    //     print ("fail");
+    // }
+
+    // TASK 02 //
+    // int a = 10;
+    // int b = 20;
+    // bool check_1 = (a < 50); // true 
+    // bool check_2=(a < b); // true
+    // print (check_1);
+    // print ( check_2);
+    // // variables changed
+    // int a_1 = 60;
+    // int b_1 = 70 ;
+    // bool condition_1 = (a_1 < 50 );
+    // bool condition_2 = ( a_1 < b_1);
+
+    // print (condition_1);
+    // print (condition_2);
+
+    // // TASK 03 //
+
+    // num subject_1= 78;
+    // num subject_2 = 45;
+    // num subject_3 = 62;
+    // num total_obtained_marks= subject_1 + subject_2 + subject_3;
+    // num total_marks = 300;
+    // num percentage = total_obtained_marks * 100 / total_marks;
+    // print (total_obtained_marks);
+    // print (total_marks);
+    // print("Robert");
+    // print(percentage);  
+
+    // TASK 04 //
+
+    // int urdu = 70;
+    // int english = 85;
+    // int maths = 60; 
+    // int pst = 55;
+    // num total_marks_obtian = urdu + english + maths + pst;
+    // num t_mark = 400;
+    // num per = total_marks_obtian * 100 / t_mark;
+
+    // double max_per = 100;
+
+    // if (max_per > 80){
+    //     print( "Grade : A+ ");
+
+    // } 
+    // else if (max_per >70){
+    //     print ("Grade : A ");
+    // }
+    // else if (max_per > 60){
+    //     print ("Grade B ");
+    // }
+    // else if (max_per > 55){
+    //     print ( "Grade : C ");
+    // }
+    // else {
+    //     print ("fail");
+    // }
+    // print ( total_marks_obtian);
+    // print (t_mark);
+    // print (per);
+
+
+    //      TASK 05      //
+    // num adult = 18;
+    // num man_1 = 17;
+    // num man_2 = 40; 
+    // if (adult > man_1  ){
+    //     print ( "Youngest");
+    // }
+    // else if (adult < man_2 ){
+    //     print (" Oldest");
+    // }
+    // else {
+    //     print("intezar karo");
+    // }
+    
+
+    // TASK 06 //
+
+    // int number = 5 ;
+    // if (number > 0 ){
+    //     print ("the number is positive");
+    // }
+    // else if (number < 0){
+    //     print (" the number is negative ");
+    // }
+    // else {
+    //     print ( "the number is zero");
+    // }
+
+    // TASK 07 //
+     
+    //  int length = 10;
+    //  int breadth = 10;
+    //  if ( length == breadth){
+    //     print ( "It is a aquare");
+    //     }
+    //     else {
+    //         print(" it is a rectangle");
+    //     }
+     
+
+     // TASK 08 //
+    num temperature = 42;
+    if (temperature < 0){
+        print ("Freezing Weather");
+    }
+    else if (temperature < 10 ){
+        print ("very cold weather");
 
     }
-    else{
-        print ("fail");
+    else if ( temperature < 20 ){
+        print ( " cold weather ");
     }
+    else if ( temperature < 30 ){
+        print ( "normal temperature");
+    }
+    else if ( temperature < 40 ){
+        print ( "its hot");
+    }
+    else if ( temperature >= 40 ){
+        print ( "its very hot");
+    }
+
+    // Assignment Compelet// 
 }
